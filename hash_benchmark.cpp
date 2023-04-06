75891899e003e49a5b46da8d1dff6318241da9ec
u175167 <u175167@s019-n015.aidevcloud>
2023-04-06 10:55:28 -0700

    Simplify argument handling

--- hash_benchmark.cpp
+++ hash_benchmark.cpp
@@ -28,18 +28,15 @@ enum algorithm {
 
 // Print the error message and exit with a failure
 [[noreturn]] void die(const char *msg);
+[[noreturn]] void die(const std::string& msg);
 
 // Print usage info to standard output
 void usage();
 
-// Parse a string into an algorithm, or nothing if it's not known
-std::optional<algorithm> get_algorithm(char *name);
-
-// Parse a number into a u64, or nothing if it's invalid
-std::optional<u64> get_number(char *str);
-
-// Parse a string into a runner, or nothing if it's invalid
-std::optional<runner> get_runner(char *str);
+// Parse an argument to the appropriate type, or exit if it's invalid
+algorithm parse_arg_algorithm(char **argv, int argidx);
+u64 parse_arg_u64(char **argv, int argidx);
+runner parse_arg_runner(char **arg, int argidx);
 
 // Run an iteration of the loop
 void run_hash(u64 i, algorithm f, char *output_buf);
@@ -47,9 +44,6 @@ void run_hash(u64 i, algorithm f, char *output_buf);
 // Run many iterations with SYCL
 template<class Selector> void run_hashes_sycl(u64 iterations, hash_fn f,
 	Selector selector, unsigned char *output_buf);
-
-// Take the value or die with message
-template<class T> T extract_or_die(std::optional<T> o, const char *msg);
 
 const char tab = '\t';
 
@@ -72,6 +66,12 @@ die(const char *msg)
 	std::exit(1);
 }
 
+[[noreturn]] void
+die(const std::string& msg)
+{
+	die(msg.c_str());
+}
+
 void
 usage()
 {
@@ -81,46 +81,38 @@ usage()
 	std::cout << "runners: serial sycl-cpu sycl-gpu" << std::endl;
 }
 
-std::optional<algorithm>
-get_algorithm(char *name)
+algorithm
+parse_arg_algorithm(char **argv, int i)
 {
-	if (std::strcmp(name, "sha224") == 0) {
-		return std::optional(SHA224);
+	if (std::strcmp(argv[i], "sha224") == 0) {
+		return SHA224;
 	}
-	return std::optional<algorithm>();
+	die("argument " + std::to_string(i) + " should be a known hash algorithm");
 }
 
-std::optional<runner>
-get_runner(char *name)
+runner
+parse_arg_runner(char **argv, int i)
 {
-	if (std::strcmp(name, "serial") == 0) {
-		return std::optional(SERIAL_RUNNER);
+	if (std::strcmp(argv[i], "serial") == 0) {
+		return SERIAL_RUNNER;
 	}
-	if (std::strcmp(name, "sycl-cpu") == 0) {
-		return std::optional(SYCL_CPU_RUNNER);
+	if (std::strcmp(argv[i], "sycl-cpu") == 0) {
+		return SYCL_CPU_RUNNER;
 	}
-	if (std::strcmp(name, "sycl-gpu") == 0) {
-		return std::optional(SYCL_GPU_RUNNER);
+	if (std::strcmp(argv[i], "sycl-gpu") == 0) {
+		return SYCL_GPU_RUNNER;
 	}
-	return std::optional<runner>();
+	die("argument " + std::to_string(i) + " should be a known runner");
 }
 
-std::optional<u64>
-get_number(char *str)
+u64
+parse_arg_u64(char **argv, int i)
 {
 	char *end;
-	u64 result = std::strtoull(str, &end, 0);
+	u64 result = std::strtoull(argv[i], &end, 0);
 	if (*end != '\0')
-		return std::optional<algorithm>();
+		die("argument " + std::to_string(i) + " should be an natural number");
 	return result;
-}
-
-template<class T> T
-extract_or_die(std::optional<T> o, const char *msg)
-{
-	if (o)
-		return *o;
-	die(msg);
 }
 
 void
@@ -155,12 +147,9 @@ main(int argc, char *argv[])
 		usage();
 		return 1;
 	}
-	u64 num_hashes = extract_or_die(get_number(argv[1]),
-		"first argument must be an unsigned integer");
-	algorithm alg = extract_or_die(get_algorithm(argv[2]),
-		"second argument must be a known algorithm");
-	runner r = extract_or_die(get_runner(argv[3]),
-		"third argument must be a known runner");
+	u64 num_hashes = parse_arg_u64(argv, 1);
+	algorithm alg = parse_arg_algorithm(argv, 2);
+	runner r = parse_arg_runner(argv, 3);
 
 	std::cout << "hashes =" << tab << num_hashes << tab;
 	std::cout << "algo =" << tab << algorithm_name[alg] << tab;
